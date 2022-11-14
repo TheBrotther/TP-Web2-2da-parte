c5844eb596e2193b9a09b65f7afb2bf23bf8a819
Lucas Quiroga <quiroga9409@gmail.com>
2022-11-13 22:42:38 -0300

    actualizo base de datos

--- DataBase/tpweb2.sql
+++ DataBase/tpweb2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 10-10-2022 a las 21:52:41
+-- Tiempo de generación: 14-11-2022 a las 02:37:56
 -- Versión del servidor: 10.4.25-MariaDB
 -- Versión de PHP: 8.1.10
 
@@ -40,8 +40,13 @@ CREATE TABLE `account` (
 --
 
 INSERT INTO `account` (`id_account`, `id_client`, `amount`, `type_account`, `coin`) VALUES
-(6, 1, 2147483647, 'Caja de ahorro', 'Dolar Estadounidense'),
-(10, 2, 5000000, 'Cuenta corriente', 'Dolar Estadounidense');
+(1, 2, 500000, 'Cuenta Corriente', 'Dolar'),
+(2, 1, 10000, 'Caja De Ahorro', 'Dolar'),
+(3, 3, 100000, 'Plazo Fijo', 'Peso'),
+(4, 1, 100000, 'Plazo Fijo ', 'Dolar'),
+(5, 2, 600000, 'Cuenta Corriente', 'Peso'),
+(12, 1, 60000000, 'Cuenta Corriente', 'Dolar '),
+(14, 9, 5000000, 'Cuenta Corriente', 'Peso');
 
 -- --------------------------------------------------------
 
@@ -61,9 +66,10 @@ CREATE TABLE `client` (
 --
 
 INSERT INTO `client` (`id_client`, `dni`, `alias`, `city`) VALUES
-(0, 38178273, 'Lucas', 'Tandil'),
-(1, 41258326, 'Carreto', 'Gesell'),
-(2, 42136985, 'Marcos', 'Gesell');
+(1, 38178273, 'Lucas', 'Tigre'),
+(2, 42630319, 'Duana', 'Rauch'),
+(3, 41690992, 'Lulo', 'Gesell'),
+(9, 42136213, 'Roberto', 'Balcarse');
 
 -- --------------------------------------------------------
 
@@ -85,9 +91,7 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id_user`, `id_client`, `username`, `pass`, `email`, `rol`) VALUES
-(8, 0, 'Lucas', '$2y$10$a4pDdQtUFe2p4Yupj1PcfeMmuwABr72AwUCXZQQAG2ubJLfTMRD8e', 'quiroga9409@gmail.com', 'admin'),
-(9, 1, 'Carretto', '$2y$10$A0usUMUTK.fcmOP.0FT/ZOnVeb0BKlE8i3ErqOPeFzSAZ1xMw1eH.', 'carretto_leonardo@gmail.com', 'normal'),
-(10, 2, 'Marcos', '$2y$10$8c9bb93nvbCT3pSUTG09dujlopp669ZrAM/u./4fDAnj/0CAT73gS', 'marcos_fumador@gmail.com', 'normal');
+(12, 1, 'Lucas', '$2y$10$3YsxUGk332tmf20L3qCWaeF6482toCQcH9gegC2zDVZX4gcnyFjSC', 'quiroga9409@gmail.com', 'admin');
 
 --
 -- Índices para tablas volcadas
@@ -121,13 +125,19 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT de la tabla `account`
 --
 ALTER TABLE `account`
-  MODIFY `id_account` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_account` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT de la tabla `client`
+--
+ALTER TABLE `client`
+  MODIFY `id_client` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de la tabla `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- Restricciones para tablas volcadas
@@ -137,13 +147,13 @@ ALTER TABLE `user`
 -- Filtros para la tabla `account`
 --
 ALTER TABLE `account`
-  ADD CONSTRAINT `account_ibfk_1` FOREIGN KEY (`id_client`) REFERENCES `client` (`id_client`);
+  ADD CONSTRAINT `fk_client` FOREIGN KEY (`id_client`) REFERENCES `client` (`id_client`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `user`
 --
 ALTER TABLE `user`
-  ADD CONSTRAINT `fk_client` FOREIGN KEY (`id_client`) REFERENCES `client` (`id_client`);
+  ADD CONSTRAINT `fk_user` FOREIGN KEY (`id_client`) REFERENCES `client` (`id_client`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
